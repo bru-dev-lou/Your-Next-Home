67dc8d0e901bbd3bb9d6e31d9ce92db99e54dbc2
Bruno Lourenco <brunolourenco1993@gmail.com>
2026-03-10 19:08:49 +0700

    Main table added

---
+++ schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS proprtiy_list {
+    id INTEGER PRIMARY KEY,
+    type TEXT NOT NULL,
+    street TEXT NOT NULL,
+    city TEXT NOT NULL,
+    post_code TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    no_bedrooms INTEGER NOT NULL,
+    no_bathrooms INTEGER NOT NULL,
+    size INTEGER NOT NULL,
+    garden BOOLEAN NOT NULL,
+    balcony BOOLEAN NOT NULL,
+    summary TEXT NOT NULL DEFAULT 'Seems like a description is missing for this property. Please contact the owner for more details.If you are the owner, please update the property description.',
+    owner_id INTEGER NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES users(id),
+    date_listed TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+}
