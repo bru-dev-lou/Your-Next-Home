00865342a8799964576426e366776a7a02511fdf
Bruno Lourenco <brunolourenco1993@gmail.com>
2026-03-15 16:02:37 +0700

    Replaced packages. Updated server.js. Added new route.

---
+++ Routes/properties.js
@@ -0,0 +1,13 @@
+import express from "express";
+import Database from "better-sqlite3";
+
+const router = express.Router();
+const db = new Database("./Database/database.db");
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const data = db.prepare("SELECT * FROM property_list WHERE id = ?").get(id);  
+  res.status(200).json(data);
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import propRouter from './Routes/properties.js';
 const app = express(); 
 const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server is working!"); 
+})
+
+app.use("/properties", propRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
