f76e059b14b70cdbadf5b7cd7061f1f177c77057
Bruno Lourenco <brunolourenco1993@gmail.com>
2026-03-18 20:03:29 +0700

    chore: Change framework + variant for client folder. feat: Add new search bar feature with auto-complete

---
+++ client/your-next-home/src/App.tsx
@@ -0,0 +1,14 @@
+import SearchBar from "./components/SearchBar"
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Welcome to my React App!</h1>
+      <p>This is a simple React application.</p>
+      <SearchBar />
+    </div>
+  )
+}
+
+export default App
