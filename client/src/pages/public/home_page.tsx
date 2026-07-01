5cc4978b125cbdd62197d7ef4ca6168a24acc0aa
Bruno Lourenco <brunolourenco1993@gmail.com>
2026-07-01 18:13:11 +0700

    feat: began styling repo, homepage done

--- client/src/pages/public/home_page.tsx
+++ client/src/pages/public/home_page.tsx
@@ -1,13 +1,16 @@
 import HomePageSearchBar from "../../components/public/homepage_searchbar_comp";
+import homePagePhoto from "../../assets/home_page_photo.png"
+import "../public/home_page.css";
 
 function HomePage() {
 
   return (
-      <div>
-        <h2>Your Next Home</h2>
-        <p>Find your dream home with us!</p>
-        <br />
-        <HomePageSearchBar />
+      <div className="main_page_container">
+        <div className="search_box_container">
+          <h2 className="h2_font">Your journey starts here!</h2>
+          <HomePageSearchBar />
+        </div>
+        <img className="home_page_photo" src={homePagePhoto}></img>
       </div>
   )
 }
