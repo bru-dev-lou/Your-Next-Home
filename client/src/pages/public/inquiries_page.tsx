092aaba649c60756f1efdc6dcd66c3f08627598f
Bruno Lourenco <brunolourenco1993@gmail.com>
2026-07-11 19:03:32 +0700

    refactor + style: inquiries page CSS Modules migration, layout updates and bug fix

--- client/src/pages/public/inquiries_page.tsx
+++ client/src/pages/public/inquiries_page.tsx
@@ -1,10 +1,12 @@
 import Inquiries from "../../components/public/inquiries_comp";
+import inquiriesPhoto from "../../assets/inquiries_page_photo.jpg";
+import styles from "../public/inquiries_page.module.css";
 
 function InquiriesPage () {
     return (
-        <div>
-            <h2>Get in touch with us!</h2>
-            <Inquiries />
+        <div className={styles.main_container}>
+            <Inquiries />   
+            <img className={styles.inquiries_photo} src={inquiriesPhoto}></img>
         </div> 
     )
 }
